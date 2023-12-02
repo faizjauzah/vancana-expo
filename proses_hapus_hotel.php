b7f642243316c223c9d7bc70381777598debd61c
Faiz Jauzah <mfaizjauzah24@gmail.com>
2023-12-03 00:01:16 +0700

    benerin

--- proses_hapus_hotel.php
+++ proses_hapus_hotel.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $id = $_GET['id'];
 session_start();
@@ -18,25 +17,4 @@ echo "<script>
 	alert ('Hotel berhasil dihapus');
 	</script>";
 echo"<meta http-equiv=refresh content=0;url=hotel.php>";
-=======
-<?php
-$id = $_GET['id'];
-session_start();
-include "koneksi.php";
-date_default_timezone_set('Asia/Bangkok');
-$target_dir = "img/";
-$get_data = mysqli_query($conn,"SELECT gambar_hotel FROM hotel where id_hotel=$id");
-$hasil = mysqli_fetch_array($get_data, MYSQLI_ASSOC);
-if($hasil['gambar_hotel']<>"-")
-{
-    unlink("img/".$hasil['gambar_hotel']);
-}
-
-
-$query=mysqli_query($conn,"DELETE from hotel where id_hotel=$id");
-echo "<script>
-	alert ('Hotel berhasil dihapus');
-	</script>";
-echo"<meta http-equiv=refresh content=0;url=hotel.php>";
->>>>>>> c3ce5c2fffbc273673df60b01429287ca52e608c
 ?>
