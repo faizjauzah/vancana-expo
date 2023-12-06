9c69fa9d7d223026ef5f15281b22cc731c918e7d
Faiz Jauzah <mfaizjauzah24@gmail.com>
2023-12-06 18:05:18 +0700

    fix last line php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include "koneksi.php";
     <nav class="navbar fixed-top navbar-expand-lg navbar-light" id="navbar">
       <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="index.php">
-          VANCANA
+          VANCANA          
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -351,4 +351,4 @@ include "koneksi.php";
   </body>
 </html>
 
-<? php mysqli_close($conn); ?>
+<?php mysqli_close($conn); ?>
