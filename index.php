501b99d20809617ad5c65c40134958fed9ac6246
Faiz Jauzah <mfaizjauzah24@gmail.com>
2023-12-06 13:25:49 +0700

    ganti VANCANA jadi vancana

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include "koneksi.php";
     <nav class="navbar fixed-top navbar-expand-lg navbar-light" id="navbar">
       <div class="container-fluid">
         <a class="navbar-brand fw-bold" href="index.php">
-          VANCANA
+          Vancana
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
